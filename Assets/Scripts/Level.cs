6ffe87c06ab63e3d0e8e8166208cbce088327786
5tarlight <developerstarlight@gmail.com>
2021-07-25 13:20:14 +0900

    level scroll

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+  public Scrollbar scrollbar;
+  private Text text;
+  public float level;
+
+  private void Awake() {
+    this.text = GetComponent<Text>();
+    level = 0f;
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    scrollbar.value = 0;
+  }
+
+    // Update is called once per frame
+  void Update()
+  {
+    level = Small(Mathf.Floor(scrollbar.value * 10) + 1, 10f);
+    this.text.text = level.ToString();
+  }
+
+  private float Small(float a, float b) {
+    if (a > b) return b;
+    else return a;
+  }
+}
